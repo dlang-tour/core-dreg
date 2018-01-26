5523164b9c77b840145da67db228a66d33be0c6f
Sebastian Wilzbach <seb@wilzba.ch>
2018-01-26 11:34:32 +0100

    Allow passing args with as additional arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,15 @@ set -u
 set -o pipefail
 
 cd /sandbox
-echo "$*" | base64 -d > onlineapp.d
+echo "$1" | base64 -d > onlineapp.d
 
-exec timeout -s KILL ${TIMEOUT:-60} dreg dmd -run onlineapp.d | tail -n100
+args=${DOCKER_FLAGS:-""}
+# fallback to $2
+args=${args:-${@:2}}
+compiler="dmd"
+
+if [[ $args =~ .*-c.* ]] ; then
+    exec timeout -s KILL ${TIMEOUT:-30} dreg "${compiler}" $args onlineapp.d | tail -n100
+elif [ -z ${2:-""} ] ; then
+    exec timeout -s KILL ${TIMEOUT:-30} dreg "${compiler}" $args -g -run onlineapp.d | tail -n10000
+fi
